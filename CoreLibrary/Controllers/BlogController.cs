e5f42067cc93e9f1a183a228c232891867f5241e
BatuhanB <balibatuhan19@gmail.com>
2022-02-26 00:08:56 +0300

    solved some errors

--- CoreLibrary/Controllers/BlogController.cs
+++ CoreLibrary/Controllers/BlogController.cs
@@ -22,19 +22,19 @@ namespace CoreLibrary.Controllers
         public IActionResult Index()
         {
             var values = bm.GetAllListBlog();
-                return View(values);
+            return View(values);
         }
         public IActionResult BlogReadAll(int id)
         {
-            ViewBag.Id = id;    
+            ViewBag.Id = id;
             var values = bm.GetBlogListByID(id);
             return View(values);
         }
         [AllowAnonymous]
         public IActionResult BlogListByWriter()
         {
-           var values = bm.GetListWithCategoryByWriterBm(1);
-           return View(values);
+            var values = bm.GetListWithCategoryByWriterBm(1);
+            return View(values);
         }
         [HttpGet]
         public IActionResult AddBlog()
@@ -44,7 +44,7 @@ namespace CoreLibrary.Controllers
                                                    {
                                                        Text = x.CategoryName,
                                                        Value = x.CategoryID.ToString()
-                                                   }).ToList() ;
+                                                   }).ToList();
             ViewBag.cv = categoryValues;
             return View();
         }
@@ -53,6 +53,13 @@ namespace CoreLibrary.Controllers
         {
             BlogValidator validationRules = new BlogValidator();
             ValidationResult result = validationRules.Validate(blog);
+            List<SelectListItem> categoryValues = (from x in cm.GetListT()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID.ToString()
+                                                   }).ToList();
+            ViewBag.cv = categoryValues;
             if (result.IsValid)
             {
                 blog.BlogStatus = true;
@@ -68,14 +75,42 @@ namespace CoreLibrary.Controllers
                     ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
                 }
             }
+            
             return View();
         }
         public IActionResult DeleteBlog(int id)
         {
             var blog = bm.GetTByID(id);
-            //bm.TDelete(blog);
-            blog.BlogStatus = false;
+            bm.TDelete(blog);
+            //blog.BlogStatus = false;
             return RedirectToAction("BlogListByWriter");
         }
+        [HttpGet]
+        public IActionResult GetBlog(int id)
+        {
+            List<SelectListItem> categoryValues = (from x in cm.GetListT()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID.ToString()
+                                                   }).ToList();
+            ViewBag.cv = categoryValues;
+            var blogValue = bm.GetTByID(id);
+            return View(blogValue);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateBlog(Blog blog)
+        {
+            //var blogs = bm.GetTByID(blog.BlogID);
+            //blogs.BlogTitle = blog.BlogTitle;
+            //blogs.BlogImage = blog.BlogImage;
+            //blogs.BlogThumbnailImage = blog.BlogThumbnailImage;
+            //blogs.Category.CategoryID = blog.Category.CategoryID;
+            //blogs.BlogContent = blog.BlogContent;
+            bm.TUpdate(blog);
+            return RedirectToAction("BlogListByWriter");
+        }
+
     }
 }
